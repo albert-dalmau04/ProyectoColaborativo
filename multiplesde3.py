b80d9a1cf9dc7606ef14687813c9cddbc8ad4816
albert-dalmau04 <CG2024453@365.stucom.com>
2024-10-01 17:10:08 +0200

    codi fet

---
+++ multiplesde3.py
@@ -0,0 +1,21 @@
+num = 0
+
+while num != 100:
+
+    num += 1
+
+    if num%3 == 0 and num%5 == 0:
+        print("CocaCola")
+
+    elif num%3 == 0:
+
+        print("Coca")
+
+    elif num%5 == 0:
+
+        print("Cola")
+
+    else:
+        print(num)
+
+        
